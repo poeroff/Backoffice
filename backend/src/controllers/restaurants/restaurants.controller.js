56036fcf3bb157f3a4a510a744eb45cebeb2bbbd
kim <iufan1526@gmail.com>
2023-12-12 17:08:04 +0900

    음식점 관련 기능 세팅

---
+++ backend/src/controllers/restaurants/restaurants.controller.js
@@ -0,0 +1,10 @@
+export class RestaurantsController {
+    /**
+     * 음식점 등록 API
+     * @param {*} req
+     * @param {*} res
+     */
+    createRestaurant = (req, res) => {
+        res.json({ 테스트: '테스트' });
+    };
+}
--- backend/src/routers/restaurants.router.js
+++ backend/src/routers/restaurants.router.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
+import { RestaurantsController } from '../controllers/restaurants/restaurants.controller.js';
 
 const router = Router();
+const restaurantsController = new RestaurantsController();
 
-router.post('/restaurants');
+router.post('/restaurants', restaurantsController.createRestaurant);
 
 export default router;
