2bd2beacddbc1c3b41579a01efcb70618f5cf83e
kim <iufan1526@gmail.com>
2023-12-15 15:54:30 +0900

    주문 확인, 주문완료 기능 추가

--- backend/src/repositories/orders/orders.repository.js
+++ backend/src/repositories/orders/orders.repository.js
@@ -1,1 +1,97 @@
-export default class OrdersRepository {}
+import prisma from '../../utiles/prisma/prisma.js';
+
+export default class OrdersRepository {
+    /**
+     * 음식점 주문 현황 조회 (사장님)
+     * @param {*} restaurantId
+     */
+    getOrders = async restaurantId => {
+        try {
+            const getOrders = await prisma.orders.findMany({
+                where: {
+                    restaurantId: +restaurantId,
+                },
+                include: {
+                    member: {
+                        select: {
+                            id: true,
+                            nickname: true,
+                        },
+                    },
+                    restaurant: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+                    menu: {
+                        select: {
+                            id: true,
+                            name: true,
+                            price: true,
+                        },
+                    },
+                },
+            });
+
+            return getOrders;
+        } catch (err) {
+            console.log(err, '오류발생!!');
+        }
+    };
+
+    /**
+     * 음식 주문 조회 (사용자)
+     */
+    getUserOrders = async memberId => {
+        try {
+            const getOrders = await prisma.orders.findMany({
+                where: {
+                    memberId: +memberId,
+                },
+                include: {
+                    member: {
+                        select: {
+                            id: true,
+                            nickname: true,
+                        },
+                    },
+                    restaurant: {
+                        select: {
+                            id: true,
+                            name: true,
+                        },
+                    },
+                    menu: {
+                        select: {
+                            id: true,
+                            name: true,
+                            price: true,
+                        },
+                    },
+                },
+            });
+
+            return getOrders;
+        } catch (err) {
+            console.log(err, '오류발생!!');
+        }
+    };
+
+    /**
+     * 주문 완료 처리
+     * @param {*} orderId
+     */
+    completeOrder = async orderId => {
+        const completeOrder = await prisma.orders.update({
+            where: {
+                id: +orderId,
+            },
+            data: {
+                state: 'COMPLETED',
+            },
+        });
+
+        return completeOrder;
+    };
+}
--- backend/src/routers/order.router.js
+++ backend/src/routers/order.router.js
@@ -3,11 +3,29 @@ import needSigninMiddleware from '../middleware/need-signin.middleware.js';
 import OrdersController from '../controllers/orders/order.controller.js';
 
 const router = Router();
-const ordersCountroller = OrdersController();
+const ordersCountroller = new OrdersController();
 
 /**
- * 주문 확인API
+ * 주문 확인API (사장님)
  */
-router.get('/orders/:restaurantId', ordersCountroller.getOrders);
+router.get(
+    '/orders/:restaurantId',
+    needSigninMiddleware,
+    ordersCountroller.getOrders
+);
+
+/**
+ * 주문 확인API (사용자)
+ */
+router.get('/orders', needSigninMiddleware, ordersCountroller.getUserOrders);
+
+/**
+ * 주문 완료 처리(사장님)
+ */
+router.patch(
+    '/orders/:restaurantId/:orderId',
+    needSigninMiddleware,
+    ordersCountroller.completeOrder
+);
 
 export default router;
